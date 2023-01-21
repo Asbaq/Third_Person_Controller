e4f72d1a833ead8f1787d040a87e5918a4251b21
Asbaq <asbaqlaareb45@gmail.com>
2023-01-21 19:53:00 +0530

    Update
    Update

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
+    // Initializing the GameObject
     public float mouseSensitivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
 
     void Update()
     {
+        // For Mouse Rotation on X axis
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Initializing the GameObject
     public CharacterController controller;
     public float speed = 6f;
     public float mouseSensitivity = 10f;
     
     void Update()
     {
+        // For Player Movement in X and Z Axis
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
