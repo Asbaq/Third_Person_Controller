bb54a4a6d5c62cb3030b01f4182763926cb01661
Asbaq <asbaqlaareb45@gmail.com>
2023-01-22 04:15:45 +0530

    Update
    Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Initializing the GameObject
     public CharacterController controller;
-    public float speed = 6f;
-    public float mouseSensitivity = 10f;
-    
-    void Update()
+    public Transform cam;
+    private float moveSpeed = 5f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    Vector3 moveDirection;
+
+    private void Update()
     {
-        // For Player Movement in X and Z Axis
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f ,verticalInput).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
     }
 }
